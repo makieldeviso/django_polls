722411d7482c19254e71b438d2808706137906af
fred_mark <112870142+makieldeviso@users.noreply.github.com>
2026-04-03 03:26:08 +0800

    Update url and views to use generic views

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
 from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
 import logging
 logger = logging.getLogger(__name__)
@@ -9,19 +10,35 @@ logger = logging.getLogger(__name__)
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
 
-    return render(request, "polls/index.html", context) 
+#     return render(request, "polls/index.html", context) 
     
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = Question.objects.get(pk = question_id)
-    return render(request, "polls/result.html", {"question": question })
+# def results(request, question_id):
+#     question = Question.objects.get(pk = question_id)
+#     return render(request, "polls/results.html", {"question": question })
+
+# Amended views
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResulstView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
@@ -39,4 +56,3 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args = (question.id,)))
-        
